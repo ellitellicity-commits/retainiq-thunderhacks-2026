9b763b420d4dd7d2a3701dab320bf1efe027a6bb
Eldigardo Naz <eldigardonaz@Eldigardos-MacBook-Air.local>
2026-03-17 21:38:42 -0400

    Fix frontend API to Render backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import Journey from "./pages/Journey";
 import Lifecycle from "./pages/Lifecycle";
 import "./App.css";
 
-const API = "http://localhost:5001";
+const API = "https://retainiq-thunderhacks-2026.onrender.com"
 
 export default function App() {
   const [page, setPage] = useState("dashboard");
